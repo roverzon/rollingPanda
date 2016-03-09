b1aa9bf70966eb2e56eb0a80655061268796de45
rov01 <roverzon@gmail.com>
2016-03-09 23:09:45 +0800

    add after action review questions and add style in editor panel

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,12 @@ var router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   var questions = [
-    { "question" : "yoyo" }
+    { "question" : "What was supposed to happen?" },
+    { "question" : "What did happen?"},
+    {"question" : "What are some improvements?"},
+    {"question" : "What are some sustainments?"},
+    {"question" : "What can be done to improve the training next time?"},
+    {"question" : "Closing comments"}
   ];
   res.render('index', { 
     title: 'Express',
